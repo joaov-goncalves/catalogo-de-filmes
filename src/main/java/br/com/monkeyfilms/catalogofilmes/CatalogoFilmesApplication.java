f8d49343164313cc224450d3bd5e3accfd3aa076
xeltonjunior <xeltonjunior@gmail.com>
2021-12-20 19:33:47 -0300

    Subindo base do projeto
    Criei a base do projeto utilizando o spring intializer.

---
+++ src/main/java/br/com/monkeyfilms/catalogofilmes/CatalogoFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.monkeyfilms.catalogofilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/monkeyfilms/catalogofilmes/CatalogoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.monkeyfilms.catalogofilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
