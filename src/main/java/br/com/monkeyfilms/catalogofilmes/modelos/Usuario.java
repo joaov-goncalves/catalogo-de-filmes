b75e3165d901fcb85dae302f1d95edcf1d9c37d5
johngonca <joaov.gonca@gmail.com>
2021-12-20 20:27:04 -0300

    Criacao da classe Usuario
    Ficará inativa por um tempo, mas servira de base pro xElton.

---
+++ src/main/java/br/com/monkeyfilms/catalogofilmes/modelos/Usuario.java
@@ -0,0 +1,27 @@
+package br.com.monkeyfilms.catalogofilmes.modelos;
+
+public class Usuario {
+	
+	private String login;
+	private String password;
+	private String nome;
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
